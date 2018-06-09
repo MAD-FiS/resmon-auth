61784f19cfd4ae3c949975429ed6710253cdabdc
lukasz22 <bartocha.lukasz@gmail.com>
2018-06-09 21:31:42 +0200

    improve code style for python scripts
    Change-Id: Ic156c486b0f826d61a0387e83fabde495b5def73

--- src/resources.py
+++ src/resources.py
@@ -4,10 +4,11 @@ from src.models import UserModel
 from flask_jwt_extended import create_access_token, jwt_required
 
 
-
 parser = reqparse.RequestParser()
-parser.add_argument('username', help='This field cannot be blank', required=True)
-parser.add_argument('password', help='This field cannot be blank', required=True)
+parser.add_argument('username',
+                    help='This field cannot be blank', required=True)
+parser.add_argument('password',
+                    help='This field cannot be blank', required=True)
 
 
 class UserRegistration(Resource):
@@ -15,7 +16,8 @@ class UserRegistration(Resource):
         data = parser.parse_args()
 
         if UserModel.find_by_username(data['username']):
-            return {'message': 'User {} already exists'.format(data['username'])}
+            return {'message':
+                    'User {} already exists'.format(data['username'])}
 
         new_user = UserModel(
             username=data['username'],
@@ -24,19 +26,22 @@ class UserRegistration(Resource):
 
         try:
             new_user.save_to_db()
-            access_token = create_access_token(identity=data['username'], expires_delta=False)
+            access_token = create_access_token(identity=data['username'],
+                                               expires_delta=False)
             return {
                 'message': 'User {} was created'.format(data['username']),
                 'access_token': access_token
             }
-        except:
+        except Exception:
             return {'message': 'Something went wrong'}, 500
 
     def option(self):
         resp = flask.Response("Foo bar baz")
         resp.headers['Access-Control-Allow-Origin'] = '*'
-        resp.headers['Access-Control-Allow-Headers']: 'Content-Type,Accept,Origin,Authorization'
-        resp.headers['Access-Control-Allow-Methods']: 'GET,POST,PUT,DELETE,OPTIONS'
+        resp.headers['Access-Control-Allow-Headers']:
+            'Content-Type,Accept,Origin,Authorization'
+        resp.headers['Access-Control-Allow-Methods']:
+            'GET,POST,PUT,DELETE,OPTIONS'
         return resp
 
 
@@ -46,22 +51,28 @@ class UserLogin(Resource):
         current_user = UserModel.find_by_username(data['username'])
 
         if not current_user:
-            return {'message': 'User {} doesn\'t exist'.format(data['username'])}
+            return {'message':
+                    'User {} doesn\'t exist'.format(data['username'])}
 
         if UserModel.verify_hash(data['password'], current_user.password):
-            access_token = create_access_token(identity=data['username'], expires_delta=False)
+            access_token = create_access_token(identity=data['username'],
+                                               expires_delta=False)
             return {
                 'message': 'Logged in as {}'.format(current_user.username),
                 'access_token': access_token
             }
         else:
             return {'message': 'Wrong credentials'}
+
     def option(self):
         resp = flask.Response("Foo bar baz")
         resp.headers['Access-Control-Allow-Origin'] = '*'
-        resp.headers['Access-Control-Allow-Headers']: 'Content-Type,Accept,Origin,Authorization'
-        resp.headers['Access-Control-Allow-Methods']: 'GET,POST,PUT,DELETE,OPTIONS'
+        resp.headers['Access-Control-Allow-Headers']:
+            'Content-Type,Accept,Origin,Authorization'
+        resp.headers['Access-Control-Allow-Methods']:
+            'GET,POST,PUT,DELETE,OPTIONS'
         return resp
+
 
 class SecretResource(Resource):
     @jwt_required
@@ -69,9 +80,12 @@ class SecretResource(Resource):
         return {
             'valid': True
         }
+
     def option(self):
         resp = flask.Response("Foo bar baz")
         resp.headers['Access-Control-Allow-Origin'] = '*'
-        resp.headers['Access-Control-Allow-Headers']: 'Content-Type,Accept,Origin,Authorization'
-        resp.headers['Access-Control-Allow-Methods']: 'GET,POST,PUT,DELETE,OPTIONS'
+        resp.headers['Access-Control-Allow-Headers']:
+            'Content-Type,Accept,Origin,Authorization'
+        resp.headers['Access-Control-Allow-Methods']:
+            'GET,POST,PUT,DELETE,OPTIONS'
         return resp
