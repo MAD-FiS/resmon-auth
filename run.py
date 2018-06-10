4e320c9091b6f8676ff5cff5e70293f01f4d948c
grzegorzr <straszne@gmail.com>
2018-06-10 14:34:14 +0200

    add global after_request
    Change-Id: Ifc66a365d7b510cf3d34a2a9bb9fa985242ccdf7

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
+
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
+
 api = Api(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -11,6 +13,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'some-secret-string'
 app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
 
+@app.after_request
+def apply_caching(resp):
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = \
+        'PUT, GET, POST, DELETE, OPTIONS'
+    resp.headers['Access-Control-Allow-Headers'] = \
+        'Origin, Accept, Content-Type, Authorization'
+    return resp
 
 db = SQLAlchemy(app)
 
