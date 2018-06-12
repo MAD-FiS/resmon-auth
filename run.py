edc3604f565b3a1fd7cc18bb43212a944d8ff54c
grzegorzr <straszne@gmail.com>
2018-06-12 09:42:11 +0200

    Read jwt key from file.
    Change-Id: I5386ddae37e8a971e79b8dd0513af94ac722525e

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
+import argparse
+
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+
+
+def read_key(key_path):
+    with open(key_path) as f:
+        key = f.read()
+    return key
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--key_path', type=str, default='jwt-secret-string')
+args = parser.parse_args()
 
 app = Flask(__name__)
 
@@ -10,7 +22,7 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'some-secret-string'
-app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+app.config['JWT_SECRET_KEY'] = read_key(args.key_path)
 
 
 @app.after_request
