574fde83daf4f3b87e32d855c7bc274f20e512b2
grzegorzr <straszne@gmail.com>
2018-06-12 10:10:34 +0200

    fix code style
    Change-Id: Ie35bad5b0a7413f0148b9cf3d7fab5eef82ed47c

--- test/unit_test.py
+++ test/unit_test.py
@@ -5,23 +5,22 @@ from src.resources import create_response
 
 
 class TestGeneral(unittest.TestCase):
-
     def test_hash(self):
         password = "password"
         pass_hash = UserModel.generate_hash(password)
         self.assertTrue(UserModel.verify_hash(password, pass_hash))
 
     def test_response_200(self):
-        response = create_response({'a':'a'}, 200)
+        response = create_response({'a': 'a'}, 200)
         self.assertTrue(response.status_code == 200)
 
     def test_response_404(self):
-        response = create_response({'a':'a'}, 404)
+        response = create_response({'a': 'a'}, 404)
         self.assertTrue(response.status_code == 404)
 
     def test_cors_headers(self):
         response = create_response({'a': 'a'}, 200)
-        headers_list =[]
+        headers_list = []
         for h in response.headers:
             headers_list.append(h)
 
@@ -30,6 +29,7 @@ class TestGeneral(unittest.TestCase):
                          'PUT, GET, POST, DELETE, OPTIONS') in headers_list)
         self.assertTrue(('Access-Control-Allow-Headers',
                          'Origin, Accept, Content-Type, Authorization') in headers_list)
+
 
 if __name__ == '__main__':
     unittest.main()
