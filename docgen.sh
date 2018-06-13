5b4ec175518e49c74680f3b6013979fef2d5c0bd
lukasz22 <bartocha.lukasz@gmail.com>
2018-06-13 17:31:59 +0200

    add script which generate python docs
    Change-Id: Ic56bd32894f4d31633ccca39d495a5e63be70455

---
+++ docgen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Generates python documentation. Move html files to docs dictionary
+if [ -z ${RESMONAUTHENV+x} ]; then
+    source ./resmon-auth.env
+fi
+
+pydoc3 -w $(pydoc3 -k src)
+for f in ./*.html ; do mv "$f" ./docs/ ; done
