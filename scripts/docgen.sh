ecdaf850faa0e8a85d1dfe2a8ce37c53b3df06d9
Tomasz Bajorek <tbajorek3@gmail.com>
2018-06-14 06:47:39 +0200

    Fix bugs and deployment skeleton
    Change-Id: Iac3e854734d6e9b20ad5a734e9dd9fbc98222c0f

---
+++ scripts/docgen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Generates python documentation. Move html files to docs dictionary
+if [ -z ${RESMONAUTHENV+x} ]; then
+    source ./resmon-auth.env
+fi
+
+rm -rf ./docs
+mkdir ./docs/
+
+MODULES=`find -name '*.py' | sed -e 's/\//./g' | sed -e 's/\.\.//g' \
+    | sed -e 's/\.py//g' | sed 's|.__init__||'`
+pydoc3 -w $MODULES
+for f in ./*.html ; do mv "$f" ./docs/ ; done
