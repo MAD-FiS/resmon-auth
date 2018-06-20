02ec16093a8fdd356e35c1cdbacf4eaf14216c5d
Tomasz Bajorek <tbajorek3@gmail.com>
2018-06-20 17:09:35 +0200

    Remove weird key in app config
    Change-Id: I390c4c3cb123e8770ab47fb45a45e4bd530e8c64

--- src/run.py
+++ src/run.py
@@ -4,9 +4,6 @@ from flask import Flask, jsonify
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-import hashlib
-import random
-
 
 def read_key(key_path):
     with open(key_path) as f:
@@ -27,12 +24,15 @@ api = Api(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = hashlib.sha256(
-    str(random.getrandbits(256)).encode('utf-8')
-).hexdigest()
+app.config['SECRET_KEY'] = \
+        'eea637dfa8c5da0db6740b4b5708ee7aecc1d5f9f830d1ce7ab9df480bace474d8db8484b84ee'\
+        '4b16e8fbb490e0ac519caaec508379622abe051ba2a9dcda431'
 
 if __name__ == "__main__":
-    app.config['JWT_SECRET_KEY'] = read_key(args.key_path)
+    app.config['JWT_SECRET_KEY'] = \
+        'eea637dfa8c5da0db6740b4b5708ee7aecc1d5f9f830d1ce7ab9df480bace474d8db8484b84ee'\
+        '4b16e8fbb490e0ac519caaec508379622abe051ba2a9dcda431'
+    #read_key(args.key_path)
 
 
 @app.after_request
